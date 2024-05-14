5278cc239c8b6f2893659c538016a74be7d6c44d
DanteRoyal <dwroyal02@naver.com>
2024-05-14 12:47:29 +0900

    프로젝트 생성

---
+++ src/main/java/com/flab/mame_java/MameJavaApplication.java
@@ -0,0 +1,13 @@
+package com.flab.mame_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MameJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MameJavaApplication.class, args);
+	}
+
+}
