c7d986571013412425b9ccecb4e88a0ba090b2ad
DanteRoyal <dwroyal02@naver.com>
2024-05-14 17:14:24 +0900

    프로젝트 이름 변경

--- src/main/java/com/flab/mame/MameJavaApplication.java
+++ src/main/java/com/flab/mame/MameJavaApplication.java
@@ -1,4 +1,4 @@
-package com.flab.mame_java;
+package com.flab.mame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/flab/mame/MameJavaApplicationTests.java
+++ src/test/java/com/flab/mame/MameJavaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.flab.mame_java;
+package com.flab.mame;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
