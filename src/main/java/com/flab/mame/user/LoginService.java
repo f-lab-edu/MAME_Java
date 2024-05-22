a99b54159d4290fa9ffd8d6b4c989ca5c37f0f45
DanteRoyal <dwroyal02@naver.com>
2024-05-22 12:55:40 +0900

    #4 Session Const 적용

--- src/main/java/com/flab/mame/user/LoginService.java
+++ src/main/java/com/flab/mame/user/LoginService.java
@@ -1,17 +1,25 @@
 package com.flab.mame.user;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.flab.mame.user.domain.User;
+import com.flab.mame.user.domain.UserRepository;
+import com.flab.mame.user.domain.UserSessionConst;
+import com.flab.mame.user.dto.UserLoginRequest;
 
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class LoginService {
 
 	private final HttpSession httpSession;
-
 	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
 
 	public void login(final UserLoginRequest reqeust) {
 		/*
@@ -24,7 +32,9 @@ public class LoginService {
 			throw new RuntimeException("비밀번호 틀림");
 		}
 
-		httpSession.setAttribute("userId", foundUser.getId());
+		httpSession.setAttribute(UserSessionConst.USER_ID, foundUser.getId());
+		log.info("session Id = {}", httpSession.getId());
+		log.info("userId = {}", httpSession.getAttribute(UserSessionConst.USER_ID));
 
 	}
 
