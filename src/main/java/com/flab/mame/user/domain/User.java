8695d25e4d3a9046139a80c62669715d2e2cd4e7
DanteRoyal <dwroyal02@naver.com>
2024-05-22 12:51:31 +0900

    #4 User 엔티티 연관관계 매핑

--- src/main/java/com/flab/mame/user/domain/User.java
+++ src/main/java/com/flab/mame/user/domain/User.java
@@ -1,12 +1,22 @@
-package com.flab.mame.user;
+package com.flab.mame.user.domain;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.hibernate.validator.constraints.Length;
+
+import com.flab.mame.profile.domain.Profile;
+import com.flab.mame.profileimage.domain.ProfileImage;
+import com.flab.mame.swipe.Swipe;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -36,15 +46,35 @@ public class User {
 	@Length(min = 12, max = 20, message = "비밀번호는 최소 12글자 이상, 최대 20글자 이하입니다.")
 	private String password;
 
-
 	/*
 	 *  TODO: 프로필 정보 받아오기
 	 *
 	 * */
-
+	@OneToOne
+	@JoinColumn(name = "profile_id")
+	private Profile profile;
 
 	/*
 	 * TODO : List for photos
+	 * */
+	@OneToMany(mappedBy = "user")
+	private List<ProfileImage> profileImages = new ArrayList<>();
+
+	@OneToMany(mappedBy = "swiper")
+	private List<Swipe> swipesSent = new ArrayList<>();
+
+	@OneToMany(mappedBy = "swipee")
+	private List<Swipe> swipesReceived = new ArrayList<>();
+
+	public void createProfile(Profile newProfile) {
+		this.profile = newProfile;
+	}
+
+
+
+	/*
+	 * TODO: MATCH 연관관계
+	 *
 	 * */
 
 }
